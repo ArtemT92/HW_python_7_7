f427a8d9dcd11274e4b48f7a8f5c8a0f83d7778b
Artem Trunilin <a.trunilin92@mail.ru>
2023-08-28 21:46:21 +0300

    Modifying zip_test

--- tests/test_zip.py
+++ tests/test_zip.py
@@ -1,21 +1,28 @@
 
-import zipfile
 from zipfile import ZipFile
+from conftest import RESOURCE_PATH, pdf_path, xls_path, xlsx_path, csv_path, zip_path
+
 def test_zip_file():
-    newzip = zipfile.ZipFile('../new_zip.zip', 'w')
-
-    newzip.write('resources/docs-pytest-org-en-latest.pdf', 'docs-pytest-org-en-latest.pdf')
-    newzip.write('resources/file_example_XLS_10.xls', 'file_example_XLS_10.xls')
-    newzip.write('resources/file_example_XLSX_50.xlsx', 'file_example_XLS_50.xlsx')
-    newzip.write('resources/new_csv.csv', 'new_csv.csv')
-    newzip.close()
+    with ZipFile(zip_path, 'w') as newzip:
+        newzip.write(pdf_path, 'docs-pytest-org-en-latest.pdf')
+        newzip.write(xls_path, 'file_example_XLS_10.xls')
+        newzip.write(xlsx_path, 'file_example_XLS_50.xlsx')
+        newzip.write(csv_path, 'new_csv.csv')
 
 
-    with ZipFile('../new_zip.zip', 'r') as myzip:
-        for item in myzip.namelist():
+
+    with ZipFile(zip_path, 'r') as newzip:
+
+        names = []
+        for item in newzip.namelist():
             print(item)
+            names.append(item)
 
-        assert item == ['docs-pytest-org-en-latest.pdf', 'file_example_XLS_10.xls', 'file_example_XLS_50.xlsx', 'new_csv.csv']
+        assert names == ['docs-pytest-org-en-latest.pdf', 'file_example_XLS_10.xls', 'file_example_XLS_50.xlsx', 'new_csv.csv']
+
+
+
+
 
 
 
